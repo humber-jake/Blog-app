4b08b5be014f865388fb0c82dcc9d21b7b3ec165
humber-jake <humber.jake@gmail.com>
2020-09-29 02:15:22 +0000

    Add Edit and Update routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const bodyParser = require("body-parser"),
+methodOverride   = require("method-override"),
 mongoose 		 = require("mongoose"),
 express 		 = require("express"),
 app 			 = express();
@@ -7,9 +8,12 @@ mongoose.connect("mongodb://localhost/blog_app", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
+mongoose.set("useFindAndModify", false);
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 const blogSchema = new mongoose.Schema({
 	title: String, 
@@ -24,6 +28,7 @@ app.get("/", function(req, res){
 	res.redirect("/blogs");
 });
 
+// INDEX
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err){
@@ -34,10 +39,12 @@ app.get("/blogs", function(req, res){
 	})
 });
 
+// NEW
 app.get("/blogs/new", function(req, res){
 	res.render("new");
 });
 
+// CREATE
 app.post("/blogs", function(req, res){
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){
@@ -48,6 +55,7 @@ app.post("/blogs", function(req, res){
 	})
 })
 
+// SHOW
 app.get("/blogs/:id", function(req, res){
 	Blog.findById(req.params.id, function(err, foundBlog){
 		if(err){
@@ -56,6 +64,28 @@ app.get("/blogs/:id", function(req, res){
 			res.render("show", {blog: foundBlog});
 		}
 	});
+})
+
+// EDIT
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+				res.render("edit", {blog: foundBlog});
+		}
+	});
+})
+
+// UPDATE
+app.put("/blogs/:id", function(req,res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	})
 })
 
 app.listen(3000, function(){
