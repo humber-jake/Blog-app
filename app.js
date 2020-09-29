e8b9541f188ef7339e04ab969813001423c0718b
humber-jake <humber.jake@gmail.com>
2020-09-29 00:42:01 +0000

    Initial commit, index page

---
+++ app.js
@@ -0,0 +1,39 @@
+const bodyParser = require("body-parser"),
+mongoose 		 = require("mongoose"),
+express 		 = require("express"),
+app 			 = express();
+
+mongoose.connect("mongodb://localhost/blog_app", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+const blogSchema = new mongoose.Schema({
+	title: String, 
+	image: String, 
+	body: String, 
+	created: {type: Date, default: Date.now}
+})
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req, res){
+	res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	})
+});
+
+app.listen(3000, function(){
+	console.log("Server is running!");
+})
